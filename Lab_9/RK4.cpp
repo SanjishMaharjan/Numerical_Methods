9298e8b5bb275ed53b43975962006ae9c7413f81
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2022-02-24 17:02:09 +0545

    RK4 method

---
+++ Lab_9/RK4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+float f(float x, float y)
+{
+    float Y;
+    Y = pow(x, 2) - y;
+    return Y;
+}
+int main()
+{
+    float x0, y0, xn, h;
+    float n;
+    float k1, k2, k3, k4, k;
+    cout << "Enter the intial conditions" << endl;
+    cin >> x0 >> y0;
+    cout << "Enter the final value of x" << endl;
+    cin >> xn;
+    cout << "Enter the no of sub intervals:" << endl;
+    cin >> n;
+    h = (xn - x0) / n;
+    float a = x0;
+    float b = y0;
+    for (int i = 0; i < n; i++)
+    {
+        k1 = h * f(a, b);
+        k2 = h * f(a + h / 2, b + k1 / 2);
+        k3 = h * f(a + h / 2, b + k2 / 2);
+        k4 = h * f(a + h, b + k3);
+        k = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+        b = b + k;
+        a = a + h;
+        cout << "x" << (i + 1) << "=" << a << endl
+             << "y" << (i + 1) << "=" << b << endl;
+    }
+
+    return 0;
+}
